84136e55440b492acc6ba9d2c01f405e9c0ecef7
LIN <459979178@qq.com>
2021-08-30 17:18:30 +0800

    增加koa-cors中间件

--- src/app/index.js
+++ src/app/index.js
@@ -2,12 +2,14 @@ const koa = require('koa')
 const bodyParser = require('koa-bodyparser');
 const errorHandler = require("./errorHandle")
 const useRoutes = require('../router/index')
+const cors = require('koa-cors')
 
 const app = new koa()
 
 
 
 app.use(bodyParser())
+app.use(cors())
 app.useRoutes = useRoutes;
 // app.use(userRouter.routes(),userRouter.allowedMethods())
 app.useRoutes();
